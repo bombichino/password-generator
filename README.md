4be8074f55f32dae056145196c1b2a48710b296e
bombichino <60117085+bombichino@users.noreply.github.com>
2020-03-01 23:13:45 -0500

    Upload of project materials

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # password-generator
 
+## The assignment
+
+Create javascript that will run on a given index.html that will generate a random password as per the described user story and criteria below.
+
+## Chief Execution/Points of note
+
+-Following the skeleton of original javascript provided, fleshed out all of the functions and variables to meet the criteria.
+-In order to guarantee that the password was randomized with random chance for character selection while still guaranteeing the password contain at least one of the required character types, I first pushed one of each of the required characters, chosen at random to an array. -Next, I created a combined array consisting of character types selected in the criteria settings.
+-Then, the combined array of accepted characters was drawn from at random to fill the desired length of password. This was shuffled as an array, then written as a string to the web page.
+
+## Provided Assignment Criteria:
+
+Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
 User Story
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
--- script.js
+++ script.js
@@ -1,4 +1,97 @@
 // create an array for each option, uppercase letters, lowercase letters, special characters
+
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArray = ["a","b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialArray = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", "<", ",", ".", ">", "?", "/"]
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var generateBtn = document.querySelector("#generate");
+
+// set password criteria, requiring a password length to be between 8 and 128 characters long, not accepting numbers above or below or choices not an integer between 8 and 128; use of lower case, upper case, number (random), and special characters.
+function generatePassword (){
+var passwordLength = parseInt(prompt("Choose a password length (a number between 8 and 128 characters; password will have a minimum of 8 characters)."));
+while (Number.isInteger(passwordLength) !== true || passwordLength < 8 || passwordLength > 128){  
+  if (Number.isInteger(passwordLength) !== true){
+      alert("You must enter a number between 8 and 128.");
+      var passwordLength = parseInt(prompt("Choose a password length (a number between 8 and 128 characters)."));
+  }
+  else if (passwordLength <8 ) {
+    alert("You must enter a number between 8 and 128.");
+      var passwordLength = parseInt(prompt("Choose a password length (a number between 8 and 128 characters)."));
+  }
+  else if (passwordLength > 128) {
+    alert("You must enter a number between 8 and 128.");
+      var passwordLength = parseInt(prompt("Choose a password length (a number between 8 and 128 characters)."));
+  }
+}
+var useLowerCase = confirm("Press 'OK' if you would like lowercase letters to be included, 'Cancel' if not.");
+var useUpperCase = confirm("Press 'OK' if you would like uppercase letters to be included, 'Cancel' if not.");
+var useNumbers = confirm("Press 'OK' if you would like numbers to be included, 'Cancel' if not.");
+var useSpecials = confirm("And lastly, press 'OK' if you would like special characters to be included, 'Cancel' if not.");
+var passwordArray = []
+var passwordFillerArray = []
+console.log(passwordLength, useLowerCase, useUpperCase, useNumbers, useSpecials);
+
+// setting up basic required characters that must be present - if each criteria is chose, push at least one character type to the password array to guarantee its presence
+// if each criteria is met, push its array into a password filler array from which the rest of the password will be chosen at random
+if (useLowerCase){
+  passwordArray.push(lowerArray[Math.floor(Math.random() * lowerArray.length)]);
+  passwordFillerArray = passwordFillerArray.concat(lowerArray);
+  console.log(passwordFillerArray);
+}
+if (useUpperCase){
+  passwordArray.push(upperArray[Math.floor(Math.random() * upperArray.length)]);
+  passwordFillerArray = passwordFillerArray.concat(upperArray);  console.log(passwordFillerArray);
+}
+if (useSpecials){
+  passwordArray.push(specialArray[Math.floor(Math.random() * specialArray.length)]);  
+  passwordFillerArray = passwordFillerArray.concat(specialArray)
+  console.log(passwordFillerArray);
+}
+if (useNumbers){
+  passwordArray.push(Math.floor(Math.random() * 10));
+  passwordFillerArray = passwordFillerArray.concat(numberArray);
+}
+console.log(passwordFillerArray);
+
+// with the password filler array comprised of 1 of any character type chosen by the criteria, use to fill at random for the remainder of the chosen password length
+while (passwordArray.length < passwordLength){
+  for (var i = passwordArray.length; i < passwordLength; i++){ 
+    passwordArray.push(passwordFillerArray[Math.floor(Math.random() * passwordFillerArray.length)]);
+  }
+  console.log(passwordArray);
+
+
+//Fisher-Yates Shuffle to randomize the filled password array
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+shuffle(passwordArray);
+console.log(passwordArray);
+
+// change password to string, console.log to confirm for debugging
+// append password to page
+var passwordResult = passwordArray.join("");
+console.log(passwordResult);
+console.log(typeof passwordResult);
+var passwordText = document.querySelector("#password");
+
+  passwordText.textContent = passwordResult;
+}
+}
+
+// call the functions and generate password
+generateBtn.addEventListener("click", generatePassword);
+
+
+
+// Pseudocode from class for reference:
 // conditional statements to alllow for when someone selects no
 // prompts and conditionals
 // Math.random
@@ -26,16 +119,9 @@
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+// // Write password to the #password input
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
